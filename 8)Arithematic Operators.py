f6580cda3a7d0cc592e43ca40da7fd3348c70016
VOIDRLM <varjun609@gmail.com>
2023-09-04 02:40:47 +0530

    Arithematic operators

---
+++ 8)Arithematic Operators.py
@@ -0,0 +1,51 @@
+# Addition (+):
+x = 5
+y = 3
+result = x + y
+print(result)
+
+# Subtraction (-):
+x = 5
+y = 3
+result = x - y
+print(result)
+
+# Multiplication (*):
+x = 5
+y = 3
+result = x * y
+print(result)
+
+
+# Division (/):
+x = 10
+y = 2
+result = x / y
+print(result)
+
+# Integer Division (//):
+x = 10
+y = 3
+result = x // y
+print(result)
+
+# Modulo (%):
+x = 10
+y = 3
+result = x % y
+print(result)
+
+# Negation
+x = 5
+result = -x
+print(result)
+
+# Absolute value
+x = -7
+result = abs(x)
+print(result)
+
+# Rround off
+x = 3.14159
+result = round(x, 2)
+print(result)
