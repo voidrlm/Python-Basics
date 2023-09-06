3b0ead01a45c3a0018c8db00d2127fbd136ccaa7
VOIDRLM <varjun609@gmail.com>
2023-09-06 19:09:28 +0530

    Arrays

---
+++ 16)Array.py
@@ -0,0 +1,13 @@
+stocks = ["AMD", "NVDA", "TSLA"]
+print(stocks)
+
+print(stocks[0])
+print(len(stocks))  # length
+
+
+# add items
+stocks.append("GME")
+print(stocks)
+# remove
+stocks.pop(3)
+print(stocks)
