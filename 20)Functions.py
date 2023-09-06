5f479d5d22179be0ad01ac1fdd59065bd2efbd53
VOIDRLM <varjun609@gmail.com>
2023-09-06 19:10:15 +0530

    Functions

---
+++ 20)Functions.py
@@ -0,0 +1,12 @@
+def methodWithoutArgument():
+    print("Hello world")
+
+
+methodWithoutArgument()
+
+
+def methodWithArgument(text):
+    print(text + " from argument")
+
+
+methodWithArgument("hello world")
